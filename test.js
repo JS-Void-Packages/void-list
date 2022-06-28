f5f6bad48ea94b66ce907d699509f6a1148a76d1
Mrthomas20121 <thomas77300@gmail.com>
2022-06-28 08:21:19 +0200

    Updated Tests

--- test.js
+++ test.js
@@ -8,7 +8,12 @@ list.add("Lazy")
 list.add("Foxes")
 list.add("Over")
 list.add("Rainbow")
+// shuffle
+console.log(list.toString())
+list.shuffle()
+console.log(list.toString())
+// remove elemnts from the array
 console.log(list.get(2))
 console.log(list.remove(2))
 console.log(list.get(2))
-console.log(List.fromArray(["Lazy", "Foxes", "Over", "Rainbow"]).toString())
+console.log(list.indexOf("Lazy"))
