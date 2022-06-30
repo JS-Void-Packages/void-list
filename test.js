66cc1a087e97e6174556f7f947da680e18300ca9
Mrthomas20121 <thomas77300@gmail.com>
2022-06-30 22:09:24 +0200

    Update test.js

--- test.js
+++ test.js
@@ -8,12 +8,17 @@ list.add("Lazy")
 list.add("Foxes")
 list.add("Over")
 list.add("Rainbow")
+
+console.log(list.get(2))
+console.log(list.remove(2))
+console.log(list.indexOf("Lazy"))
+
 // shuffle
 console.log(list.toString())
 list.shuffle()
 console.log(list.toString())
-// remove elemnts from the array
-console.log(list.get(2))
-console.log(list.remove(2))
-console.log(list.get(2))
-console.log(list.indexOf("Lazy"))
+
+// reverse the list
+list.reverse()
+console.log(list.toString())
+console.log(list.getAllIndexes().toString())
